a2d5767aab727dc5c3765a29abf9adf3599ffb17
andgineer <filbert@yandex.ru>
2020-08-16 19:50:25 +0300

    db fixture refactored as thread safe

--- backend/app/db/session.py
+++ backend/app/db/session.py
@@ -1,18 +1,62 @@
 from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker, Session
 from sqlalchemy.engine import Engine
-from functools import lru_cache
 from app.config import Config
+from sqlalchemy import event
+from typing import Optional
+import logging
 
 
-@lru_cache()
+_engine: Optional[Engine] = None  # cache
+_session_maker: Optional[sessionmaker] = None  # cache
+_injected_session_maker: Optional[sessionmaker] = None  # injection
+
+
+log = logging.getLogger()
+
+
 def engine(config: Config) -> Engine:
-    return create_engine(
+    global _engine
+    if _engine:
+        return _engine
+
+    _engine = create_engine(
             config.db_uri,
             pool_pre_ping=True,
             pool_size=config.db_pool_size,
         )
 
+    # event listeners to visualize potential connection pool leakage
+    connections_checked_out_counter = 0
+
+    @event.listens_for(_engine, 'checkout')
+    def receive_checkout(dbapi_connection, connection_record, connection_proxy):
+        nonlocal connections_checked_out_counter
+        connections_checked_out_counter += 1
+        log.debug('@'*5 + f' DB connection checkOUT. Used: {connections_checked_out_counter}')
+
+    @event.listens_for(_engine, 'checkin')
+    def receive_checkin(dbapi_connection, connection_record):
+        nonlocal connections_checked_out_counter
+        connections_checked_out_counter -= 1
+        log.debug('@'*5 + f' DB connection checkIN. Used: {connections_checked_out_counter}')
+
+    return _engine
+
+
+def session_maker(config: Config) -> sessionmaker:
+    global _session_maker
+    if not _session_maker:
+        _session_maker = sessionmaker(
+            autocommit=False,
+            autoflush=False,
+            bind=engine(config)
+        )
+    return _session_maker
+
 
 def get_session(config: Config) -> Session:
-    return sessionmaker(autocommit=False, autoflush=False, bind=engine(config))()
+    if _injected_session_maker is not None:
+        return _injected_session_maker()
+    else:
+        return session_maker(config)()
--- backend/tests/fixtures/db.py
+++ backend/tests/fixtures/db.py
@@ -1,9 +1,9 @@
 from sqlalchemy import event
 from sqlalchemy.orm import sessionmaker
 import sqlalchemy.exc
-from app.db.session import engine
+import app.db.session as app_session
 import pytest
-import config
+import app.config as app_config
 import logging
 import uuid
 
@@ -12,28 +12,52 @@ log = logging.getLogger()
 
 
 @pytest.fixture(
-    scope='function',
+    scope='session',
 )
 def db(pytestconfig):
     """
-    SQLAlchemy session for tests and for the code under tests started with SAVEPOINT.
-    After test rollback to this SAVEPOINT.
-
-    We auto-add this fixture to all tests that use fixtures `client`, `celery_app` and `celery_worker
-    but is not marked as `does_not_change_db` - see `pytest_collection_modifyitems` hook
-
-    And tests that do something directly in DB can use this fixture implicitly
+    SQLAlchemy DB session from app.db.session.
+    We need this fixture
+        1) Simple and explicit way to get DB session in a test
+        2) Raise exception if in External test mode we use unittests (with direct access to DB)
     """
+    if app_config.get_config().host is not None:
+        yield None
+        # No sense using local DB when we in External server test mode.
+        # We do not raise exception here because some tests can check External server test mode
+        # by themselves and do not use the fixture in that mode. So this is ok if some
+        # tests in External server test mode request but do not use the fixture.
+    else:
+        session = app_session.get_session(app_config.get_config())
+        yield session
+        session.close()
+
+
+@pytest.fixture(
+    scope='function',
+)
+def db_indepotent(pytestconfig):
+    """
+    DB session that roll back all DB modifications made in a test.
+    So you do not need to write per-test tear-down for DB changes made by test.
+
+    SQLAlchemy session for tests and for the code under tests started with SAVEPOINT.
+    After test DB rollback to this SAVEPOINT.
+
+    If we choose to use this approach the we better auto-add this fixture to all tests
+    that use fixtures `client` and `celery_*` and is not marked as `does_not_change_db`.
+    So to all tests that potentially could change DB in backend request handler or in Celery task.
+    To do that uncomment the code inside `pytest_collection_modifyitems` hook.
+    """
+    if app_config.get_config().host is not None:
+        yield None  # no sense using local DB when we test external Server
+        return  # stop iterations for this fixture generator
     try:
-        connection = engine(
-            config.get_test_config()
+        connection = app_session.engine(
+            app_config.get_config()
         ).connect()
     except sqlalchemy.exc.OperationalError:
-        pytest.exit(
-            f'Tests have to connect to DB \n {config.get_test_config().db_uri}\n'
-            f'Use "./up-dev.sh -d postgres" to run it.',
-            returncode=2
-        )
+        pytest.exit(f'Tests have to connect to DB {app_config.get_config().db_uri}', returncode=2)
 
     # begin a non-ORM transaction
     trans = connection.begin()
@@ -42,9 +66,14 @@ def db(pytestconfig):
     session.begin_nested()  # SAVEPOINT
     log.debug(f'[[[ savepoint transaction ]]] Start in {session.info}')
 
-    config._session = session  # Inject session to the server code under test
+    app_session._session = session  # Inject session to the server code under test
+    # todo This session should not be used in parallel by backend handlers and Celery tasks.
+    # todo So we have to implement custom session maker that wait for a mutex before giving
+    # todo this session
+    # todo And we should override `close` method in the DB session to release this mutex
+    # todo but do not actually close the session so it will be given to next waiting handler/task.
 
-    @event.listens_for(config._session, "after_transaction_end")
+    @event.listens_for(session, "after_transaction_end")
     def restart_savepoint(session, transaction):
         """
         Each time that SAVEPOINT ends, reopen it
@@ -55,7 +84,7 @@ def db(pytestconfig):
 
     yield session
 
-    config._session = None  # remove so server code in future will generate session by itself
+    app_session._session = None  # remove so server code in future will generate session by itself
     session.close()
     log.debug(f'[[[ savepoint transaction ]]] Rollback in {session.info}')
     trans.rollback()  # roll back to the SAVEPOINT

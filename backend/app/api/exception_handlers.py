3cb2b381b9527e38bdb93ce7e66410397c57f9d7
Andrei Sorokin <andrei.sorokin_ext@novartis.com>
2021-10-14 10:19:23 +0300

    Pydantic exception handler Unhandled exceptions processing in middleware

--- backend/app/api/exception_handlers.py
+++ backend/app/api/exception_handlers.py
@@ -4,12 +4,38 @@ from app.api.v1.models import GeneralErrorResponse
 from fastapi import Request
 from fastapi import status
 from fastapi.responses import JSONResponse
+from fastapi.exceptions import RequestValidationError
+from pprint import pformat
+import logging
+
+
+log = logging.getLogger()
+
+
+async def unhandled_exception_middleware(request: Request, call_next):
+    try:
+        return await call_next(request)
+    except Exception as e:
+        log.error(f"Unhandled exception {e}")
+        # todo for some app it is not secure to reveal unhandled exception to client
+        return JSONResponse(
+            content=GeneralErrorResponse(message=str(e)).dict(),
+            status_code=status.HTTP_500_INTERNAL_SERVER_ERROR,
+        )
 
 
 async def general_exception_handler(request: Request, exc: Exception):
     return JSONResponse(
         status_code=status.HTTP_400_BAD_REQUEST,
         content=GeneralErrorResponse(message=str(exc)).dict(),
+    )
+
+
+async def validation_exception_handler(request: Request, exc: RequestValidationError):
+    message = f"{pformat(exc.errors())}\n{pformat(exc.body)}"
+    return JSONResponse(
+        status_code=status.HTTP_422_UNPROCESSABLE_ENTITY,
+        content=GeneralErrorResponsemessage(message=message).dict(),
     )
 
 
--- backend/app/main.py
+++ backend/app/main.py
@@ -9,13 +9,14 @@ import app.db.session as app_session
 import sqlalchemy.exc
 import sqlalchemy.orm.exc
 from app.api.exception_handlers import db_exception_handler
-from app.api.exception_handlers import db_not_found_exception_handler
-from app.api.exception_handlers import general_exception_handler
+from app.api.exception_handlers import db_not_found_exception_handler, unhandled_exception_middleware
+from app.api.exception_handlers import general_exception_handler, validation_exception_handler
 from app.api.routing import router
 from app.api.routing import TAGS
 from app.config import API_V1_STR
 from fastapi import FastAPI
 from starlette.requests import Request
+from fastapi.exceptions import RequestValidationError
 
 log = logging.getLogger()
 
@@ -27,11 +28,12 @@ app = FastAPI(
     version="1.0.0",
     exception_handlers={
         Exception: general_exception_handler,
+        RequestValidationError: validation_exception_handler,
         sqlalchemy.exc.IntegrityError: db_exception_handler,
         sqlalchemy.orm.exc.NoResultFound: db_not_found_exception_handler,
     },
 )
-
+app.middleware("http")(unhandled_exception_middleware)
 app.include_router(router, prefix=API_V1_STR)
 
 

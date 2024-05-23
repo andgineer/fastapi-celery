cf7f3e565060aa0ac9b12e0319389b5649025a48
Andrei Sorokin <Andrei_Sorokin2@epam.com>
2024-05-23 05:58:08 +0200

    https://github.com/andgineer/fastapi-celery/issues/7 compose also can build

--- compose.sh
+++ compose.sh
@@ -3,7 +3,18 @@
 #
 # Examples:
 # compose.sh logs tests
+cleanup() {
+  rm -rf .setup-scripts
+}
+
+# Trap signals and execute the cleanup function
+trap cleanup EXIT INT TERM ERR
+cp -r ../../docker-scripts .setup-scripts
+
 docker-compose \
   -f docker-compose.yml \
   -f docker-compose-dev.yml \
   "$@"
+
+# Cleanup the .setup-scripts folder (redundant, but ensures cleanup)
+cleanup

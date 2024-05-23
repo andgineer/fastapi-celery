5095fec5b788886c607edae88d5a988b5d59b6fa
Andrei Sorokin <Andrei_Sorokin2@epam.com>
2024-05-23 05:59:16 +0200

    https://github.com/andgineer/fastapi-celery/issues/7 now build hooks inside compose

--- build.sh
+++ build.sh
@@ -7,16 +7,8 @@
 # build.sh --no-cache
 # build.sh tests
 # Function to clean up the .setup-scripts folder
-cleanup() {
-  rm -rf .setup-scripts
-}
 
-# Trap signals and execute the cleanup function
-trap cleanup EXIT INT TERM ERR
-cp -r ../../docker-scripts .setup-scripts
 ./compose.sh \
   build \
   "$@"
 
-# Cleanup the .setup-scripts folder (redundant, but ensures cleanup)
-cleanup

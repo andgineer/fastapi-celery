251b9a5380d587532a8aeb234b6731e700c78fac
Andrei Sorokin <Andrei_Sorokin2@epam.com>
2024-05-27 16:18:26 +0200

    https://github.com/andgineer/fastapi-celery/issues/7 patch celery task using context manager

--- backend/tests/test_celery_boilerplate.py
+++ backend/tests/test_celery_boilerplate.py
@@ -9,30 +9,26 @@ import app.tasks.debug
 log = logging.getLogger()
 
 
-@patch("app.tasks.debug.dummy_function")
-def test_celery_boilerplate_task_direct(
-    function_mock, celery_worker, celery_app
-):
+def test_celery_boilerplate_task_direct(celery_worker, celery_app):
     """
     Shows how to test celery tasks with test worker
     """
-    function_mock.return_value = 44
-    assert app.tasks.debug.dummy_task.delay().get(timeout=10) == 44
-    function_mock.assert_called()
+    with patch("app.tasks.debug.dummy_function") as function_mock:
+        function_mock.return_value = 44
+        assert app.tasks.debug.dummy_task.delay().get(timeout=10) == 44
+        function_mock.assert_called()
 
 
-@patch("backend.app.tasks.debug.dummy_function")
-def test_celery_boilerplate_send_task(
-    function_mock, celery_worker, celery_app
-):
+def test_celery_boilerplate_send_task(celery_worker, celery_app):
     """
     Shows how to test full celery loop - sending and executing task
     """
-    function_mock.return_value = 55
-    result: celery.result.AsyncResult = celery_app.send_task("tasks.dummy")
-    task = celery_app.AsyncResult(id=result.id)
-    while task.state == "PENDING":
-        log.info("Waiting for task..")
-        time.sleep(0.3)
-    function_mock.assert_called()
-    assert task.result == 55
+    with patch("app.tasks.debug.dummy_function") as function_mock:
+        function_mock.return_value = 55
+        result: celery.result.AsyncResult = celery_app.send_task("tasks.dummy")
+        task = celery_app.AsyncResult(id=result.id)
+        while task.state == "PENDING":
+            log.info("Waiting for task..")
+            time.sleep(0.3)
+        function_mock.assert_called()
+        assert task.result == 55

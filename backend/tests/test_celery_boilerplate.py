0d7a2564b6a47e457056ad56fa35009c8da11f56
Andrei Sorokin <Andrei_Sorokin2@epam.com>
2024-05-30 06:40:30 +0200

    https://github.com/andgineer/fastapi-celery/issues/8 patch task before celery worker started

--- backend/tests/test_celery_boilerplate.py
+++ backend/tests/test_celery_boilerplate.py
@@ -3,6 +3,7 @@ import time
 from unittest.mock import patch
 
 import celery.result
+import pytest
 
 import app.tasks.debug
 
@@ -10,22 +11,36 @@ import app.tasks.debug
 log = logging.getLogger()
 
 
-def test_celery_boilerplate_send_task(celery_session_worker, celery_session_app):
+@pytest.fixture(scope="session", autouse=True)
+def mock_dummy_function(request):
+    """
+    Patch the dummy_function in the app.tasks.debug module before the
+    Celery worker is started.
+    """
+    if request.param in [44, 55]:
+        with patch("backend.app.tasks.debug.dummy_function", return_value=request.param):
+            yield
+    else:
+        yield
+
+
+@pytest.mark.parametrize("mock_dummy_function", [55], indirect=True)
+def test_celery_boilerplate_send_task(mock_dummy_function, celery_worker, celery_app):
     """
     Shows how to test full celery loop - sending and executing task
     """
-    with patch("backend.app.tasks.debug.dummy_function", return_value=55):
-        result: celery.result.AsyncResult = celery_session_app.send_task("tasks.dummy")
-        task = celery_session_app.AsyncResult(id=result.id)
-        while task.state == "PENDING":
-            log.info("Waiting for task..")
-            time.sleep(0.3)
-        assert task.result == 55
+    result: celery.result.AsyncResult = celery_app.send_task("tasks.dummy")
+    task = celery_app.AsyncResult(id=result.id)
+    while task.state == "PENDING":
+        log.info("Waiting for task..")
+        time.sleep(0.3)
+    assert task.result == 55
 
 
-def test_celery_boilerplate_task_direct(celery_session_worker, celery_session_app):
+@pytest.mark.parametrize("mock_dummy_function", [44], indirect=True)
+def test_celery_boilerplate_task_direct(mock_dummy_function, celery_worker, celery_app):
     """
     Shows how to test celery tasks with test worker
     """
-    with patch("backend.app.tasks.debug.dummy_function", return_value=44):
-        assert app.tasks.debug.dummy_task.delay().get(timeout=10) == 44
+    assert app.tasks.debug.dummy_task.delay().get(timeout=10) == 44
+

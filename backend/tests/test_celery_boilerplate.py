a3a92baede08fff6d0753115adc3862cafc0676e
Andrei Sorokin <Andrei_Sorokin2@epam.com>
2024-05-31 06:41:12 +0200

    https://github.com/andgineer/fastapi-celery/issues/8 removed test with send task

--- backend/tests/test_celery_boilerplate.py
+++ backend/tests/test_celery_boilerplate.py
@@ -10,18 +10,19 @@ import app.tasks.debug
 log = logging.getLogger()
 
 
-def test_celery_boilerplate_send_task(celery_worker, celery_app):
-    """
-    Shows how to test full celery loop - sending and executing task
-    """
-    with patch("app.tasks.debug.dummy_function", return_value=55):
-        celery_worker.reload()
-        result: celery.result.AsyncResult = celery_app.send_task("tasks.dummy")
-        task = celery_app.AsyncResult(id=result.id)
-        while task.state == "PENDING":
-            log.info("Waiting for task..")
-            time.sleep(0.3)
-        assert task.result == 55
+# todo: flaky uses unpatched task
+# def test_celery_boilerplate_send_task(celery_worker, celery_app):
+#     """
+#     Shows how to test full celery loop - sending and executing task
+#     """
+#     with patch("app.tasks.debug.dummy_function", return_value=55):
+#         celery_worker.reload()
+#         result: celery.result.AsyncResult = celery_app.send_task("tasks.dummy")
+#         task = celery_app.AsyncResult(id=result.id)
+#         while task.state == "PENDING":
+#             log.info("Waiting for task..")
+#             time.sleep(0.3)
+#         assert task.result == 55
 
 
 def test_celery_boilerplate_task_direct(celery_worker, celery_app):

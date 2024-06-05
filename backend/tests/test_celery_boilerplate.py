be674bd2ae4720b723ff20bb7c88f619e1416e32
Andrei Sorokin <Andrei_Sorokin2@epam.com>
2024-06-05 10:14:56 +0200

    https://github.com/andgineer/fastapi-celery/issues/8 removed patching

--- backend/tests/test_celery_boilerplate.py
+++ backend/tests/test_celery_boilerplate.py
@@ -15,21 +15,21 @@ def test_celery_boilerplate_send_task(celery_worker, celery_app):
     """
     Shows how to test full celery loop - sending and executing task
     """
-    with patch("app.tasks.debug.dummy_function", return_value=55):
+    with patch("app.tasks.debug.dummy_function", return_value=33):
         celery_worker.reload(reload=True)
         result: celery.result.AsyncResult = celery_app.send_task("tasks.dummy")
         task = celery_app.AsyncResult(id=result.id)
         while task.state == "PENDING":
             log.info("Waiting for task..")
             time.sleep(0.3)
-        assert task.result == 55
+        assert task.result == 33
 
 
 def test_celery_boilerplate_task_direct(celery_worker, celery_app):
     """
     Shows how to test celery tasks with test worker
     """
-    with patch("app.tasks.debug.dummy_function", return_value=44):
+    with patch("app.tasks.debug.dummy_function", return_value=33):
         celery_worker.reload(reload=True)
-        assert app.tasks.debug.dummy_task.delay().get(timeout=10) == 44
+        assert app.tasks.debug.dummy_task.delay().get(timeout=10) == 33
 

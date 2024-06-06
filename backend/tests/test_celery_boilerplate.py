c56a441dc5b729fac1a76d7a2d30cddcbac7c8dc
Andrei Sorokin <Andrei_Sorokin2@epam.com>
2024-06-06 06:28:10 +0200

    https://github.com/andgineer/fastapi-celery/issues/8 use monkeypatch in only one boilerplate test

--- backend/tests/test_celery_boilerplate.py
+++ backend/tests/test_celery_boilerplate.py
@@ -15,14 +15,15 @@ def test_celery_boilerplate_send_task(celery_worker, celery_app, monkeypatch):
     """
     Shows how to test full celery loop - sending and executing task
     """
-    monkeypatch.setattr(app.tasks.debug, "dummy_function", lambda: 55)
+    # todo: if we patch in both tests it does not work
+    monkeypatch.setattr(app.tasks.debug, "dummy_function", lambda: 33)
     celery_worker.reload(reload=True)
     result: celery.result.AsyncResult = celery_app.send_task("tasks.dummy")
     task = celery_app.AsyncResult(id=result.id)
     while task.state == "PENDING":
         log.info("Waiting for task..")
         time.sleep(0.3)
-    assert task.result == 55
+    assert task.result == 33
 
 
 def test_celery_boilerplate_task_direct(celery_worker, celery_app, monkeypatch):
@@ -30,6 +31,5 @@ def test_celery_boilerplate_task_direct(celery_worker, celery_app, monkeypatch):
     Shows how to test celery tasks with test worker
     """
     monkeypatch.setattr(app.tasks.debug, "dummy_function", lambda: 44)
-    celery_worker.reload(reload=True)
     assert app.tasks.debug.dummy_task.delay().get(timeout=10) == 44
 
